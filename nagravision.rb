0cf64d1d0aa2029276de097e49d6715a91f6fc22
Francois Weber <fweber.mmt-b2009@fh-salzburg.ac.at>
2011-03-24 12:40:19 +0100

    form upload

--- nagravision.rb
+++ nagravision.rb
@@ -5,3 +5,20 @@ require 'erb'
 get '/' do
   erb :index
 end
+
+post '/upload' do
+
+  @post = params[:post]
+  @numberOfBlocks = params[:post][:blocks]
+  directory = "public/files"
+
+  unless params[:post][:file] && (tmpfile = params[:post][:file][:tempfile]) && (name = params[:post][:file][:filename])
+      @error = "No file selected"
+      return "Upload Error"#haml(:upload)
+  end
+
+  path = File.join(directory, name)
+  File.open(path, "wb") { |f| f.write(tmpfile.read) }
+
+  erb :upload
+end

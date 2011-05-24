e3daf1610ff09e687f74640514fba34191a6d77c
Andre Schweighofer <dre@fidrelity.at>
2011-05-24 14:25:06 +0200

    cleanup + comments

--- nagravision.rb
+++ nagravision.rb
@@ -80,7 +80,8 @@ post '/upload' do
 
   @im1 = Image.new(@im.columns, 1)
   @im2 = Image.new(@im.columns, 1)
-  
+  @next = Image.new(@im.columns, 1)
+
   @decoded_img = Image.new(@im.columns, @im.rows)
 
   @comp_arr = Array.new(block_size) { Array.new(block_size) { Hash.new} }
@@ -88,7 +89,7 @@ post '/upload' do
   alrdy_incl = Array.new
 
 
-  #1 finde erste linie
+  # compare each line with each other within the first block
   for i in 0..(block_size - 1)
     line = @im.export_pixels(0, i, @im.columns, 1, "RGB")
     @im1.import_pixels(0,0,@im.columns, 1, "RGB", line)
@@ -103,17 +104,36 @@ post '/upload' do
         @comp_arr[i][j][:value] = diff[1]
       end
     end
+    # ENABLE this to influence 'first line choice' with next block
+    #tmp_sum = 0
+    #for k in (block_size - 1)..((block_size * 2) - 1)
+    #  nl = @im.export_pixels(0, k, @im.columns, 1, "RGB")
+    #  @next.import_pixels(0, 0, @im.columns, 1, "RGB", nl)
+    #  tmp = @im1.difference(@next)
+    #  tmp_sum += tmp[1]
+    #end
+    #for l in 0..(block_size - 1)
+    #  if i == l
+    #    next
+    #  else
+    #    puts "tmp #{tmp_sum}"
+    #    @comp_arr[i][l][:value] += tmp_sum #/ block_size
+    #  end
+    #end
+    #puts "nexxt i"
   end
 
   first_line = 0
+  # in this line we save which lines differ the most from each otehr
+  # we assume that these are the possible first and last lines of the block 
   biggest_diff = Hash.new
   biggest_diff[:diff] = 0
   biggest_diff[:line1] = 0
   biggest_diff[:line2] = 1
 
+  # now check who are the possible first/last lines
   for i in 0..(block_size - 1)
     for j in 0..(block_size - 1)
-      #puts "i: #{i} and j #{j}"
       if(i == j)
         next
       else
@@ -128,23 +148,24 @@ post '/upload' do
 
   puts "biggest diff with #{biggest_diff[:diff]}, first line: #{biggest_diff[:line1]} with #{biggest_diff[:line2]}"
 
+  # for now we just guess which of those is the first line
   first_line = biggest_diff[:line1]
-#2 finde best match für linie -> linie = betch match
-#3 finde best match für linie...
+
+
   @comp = Array.new(@im.rows) { Hash.new }
-    puts "**** #{@comp.size} and #{@im.rows}"
+  
+  # we start with our guessed first line
   line = @im.export_pixels(0, first_line, @im.columns, 1, "RGB")
   alrdy_incl << first_line
-  #puts first_line
 
+  # we now check for the best match within the block
+  # and continue to find the best match for the line that was chosen before
+  # and so on...
   for block in 0..(@numberOfBlocks.to_i - 1)
-    
     for i in (block * block_size)..(((block + 1) * block_size) - 1)
       @im1.import_pixels(0,0,@im.columns, 1, "RGB", line)
       @comp[i][:best_line] = -1
       @comp[i][:best_diff] = 100
-
-      #alrdy_incl << i
 
       for j in (block * block_size)..(((block + 1) * block_size) - 1)
         if(i == j)
@@ -156,28 +177,29 @@ post '/upload' do
           
           if diff[1] < @comp[i][:best_diff] && !alrdy_incl.include?(j)
             alrdy_incl.delete(@comp[i][:best_line])
-   #         puts "removing #{@comp[i][:best_line]}"
             alrdy_incl << j
-    #        puts j
+
             @comp[i][:best_line] = j
             @comp[i][:best_diff] = diff[1]
           end
         end
       end
+      # if no new line is found (normally because there is none left in the block) we just use the
+      # last 'best' line as next
+      # **** THIS IS NOT A GOOD SOLUTION ->  transitions between blocks look wrong
       if  @comp[i][:best_line] == -1
-        puts "*********** NO LINE FOUND, comp size: #{@comp.size}, with i #{i}"#
         @comp[i][:best_line] = @comp[(i - 1)][:best_line]
-        @comp[i][:no_line] = true
         break
       end
+      # continue with the best matched line
       line = @im.export_pixels(0, @comp[i][:best_line], @im.columns, 1, "RGB")
-      #puts "next line is #{ @comp[i][:best_line]}"
     end 
   end
 
    next_line = first_line
+   # draw the picture with the before calculated best matching lines
+   # NOTE: this may be done in the loop before too. would be a performance boost!
    for i in 0..(@comp.size - 1)
-     #puts next_line
      tmp_line = @im.export_pixels(0, next_line, @im.columns, 1, "RGB")
 
      puts "next line: #{next_line} and i #{i} and best: #{@comp[i][:best_line]}"
@@ -185,6 +207,7 @@ post '/upload' do
      next_line = @comp[i][:best_line]
    end
 
+  # write the (hopefully correctly) decoded image
   dec_path = File.join(directory, "dec_" + @fileName)
   @decoded_img.write(dec_path)
 
